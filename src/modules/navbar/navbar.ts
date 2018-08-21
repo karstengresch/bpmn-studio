1465b7690297cf0065a324de3a74cc56c8789989
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-08-21 09:21:46 +0200

    :lipstick: Improve Variable Name

--- src/modules/navbar/navbar.ts
+++ src/modules/navbar/navbar.ts
@@ -18,7 +18,11 @@ export class NavBar {
   public showTools: boolean = false;
   public disableStartButton: boolean = true;
   public disableSaveButton: boolean = false;
+<<<<<<< HEAD
   public disableDiagramUploadButton: boolean = true;
+=======
+  public showDiagramUploadButton: boolean = false;
+>>>>>>> :lipstick: Improve Variable Name
   public diagramContainsUnsavedChanges: boolean = false;
 
   private _router: Router;
